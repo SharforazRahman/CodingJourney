41bc669821a1b99a9b566fd6bc81446aa8eca2d2
SharforazRahman <111514061+SharforazRahman@users.noreply.github.com>
2023-03-30 01:10:16 +0600

    findLuckyIntegerInArray

--- src/find_lucky_integer_in_an_array.java
+++ src/find_lucky_integer_in_an_array.java
@@ -1,31 +1,22 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class find_lucky_integer_in_an_array {
     public static void main(String[] args) {
-        int[] arr = {2,2,2,3,3};
-        HashMap <Integer, Integer> hm = new HashMap<>();
-        for (int num : arr) {
-            if (hm.containsKey(num)) {
-                int old_frequency = hm.get(num);
-                int new_frequency = old_frequency + 1;
-                hm.put(num, new_frequency);
-            } else {
-                hm.put(num, 1);
-            }
+        int[] arr = {1, 2, 2, 3, 3, 3};
+        System.out.println(findLucky(arr));
+    }
+
+    public static int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(arr);
+        for (int i : arr) map.put(i, map.getOrDefault(i, 0) + 1);
+        System.out.println(map);
+        for (int j = arr.length - 1; j >= 0; j--) {
+            if (map.containsKey(arr[j]) && map.get(arr[j]) == arr[j]) return arr[j];
         }
-        Object result = 0;
-        Set<Map.Entry<Integer, Integer>> s = hm.entrySet();
-        for (Map.Entry<Integer, Integer> entry : s) {
-            if (Objects.equals(entry.getKey(), entry.getValue())) {
-                result = (entry.getKey());
-            }
-        }
-        int final_result = Integer.parseInt(result.toString());
-        if(final_result < 1){
-            System.out.println(-1);
-        }
-        else {
-            System.out.println(final_result);
-        }
+        return -1;
     }
 }
+
