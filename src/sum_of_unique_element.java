90422370ed7ad32ebd0d03690612311a29b466e2
SharforazRahman <111514061+SharforazRahman@users.noreply.github.com>
2023-03-30 01:29:45 +0600

    Sum of Unique Elements

--- src/sum_of_unique_element.java
+++ src/sum_of_unique_element.java
@@ -1,27 +1,19 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class sum_of_unique_element {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1,1};
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int[] num = {1, 1, 1, 1, 1};
+        System.out.println(sumOfUnique(num));
+    }
+
+    public static int sumOfUnique(int[] a) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : a) map.put(i, map.getOrDefault(i, 0) + 1);
         int sum = 0;
-        for (int num : nums) {
-            if (hm.containsKey(num)) {
-                int old_frequency = hm.get(num);
-                int new_frequency = old_frequency + 1;
-                hm.put(num, new_frequency);
-            } else {
-                hm.put(num, 1);
-            }
+
+        for(int i : map.keySet()){
+            if(map.get(i) == 1) sum+= i;
         }
-        for (Map.Entry entry : hm.entrySet()) {
-            int store = (int) entry.getValue();
-            if (store == 1) {
-                int key = (int) entry.getKey();
-                sum = sum + key;
-            }
-        }
-        System.out.println(sum);
+        return sum;
     }
 }
