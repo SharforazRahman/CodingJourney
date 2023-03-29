f2a35817bcae75abf3422e77efa44f639d1e40f6
SharforazRahman <111514061+SharforazRahman@users.noreply.github.com>
2023-03-30 00:36:44 +0600

    hashMapUpdateCode

--- src/most_common_word.java
+++ src/most_common_word.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 public class most_common_word {
     public static void main(String[] args) {
-        String paragraph = "a, a, a, a, b,b,b,c, c";
-        String[] ban = {"a"};
+        String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        String[] ban = {"hit"};
         System.out.println(mostCommonWord(paragraph, ban));
     }
 
@@ -14,14 +14,14 @@ public class most_common_word {
         HashMap<String, Integer> map = new HashMap<>();
         Set<String> set = new HashSet<>(Arrays.asList(ban));
 
-        for (String i : paragraph.replaceAll("\\W+", " ").toLowerCase().split("\\s+")) {
-            if (!set.contains(i)) map.put(i, map.getOrDefault(i, 0) + 1);
+        for(String i : paragraph.replaceAll("\\W+" , " ").toLowerCase().split("\\s+")){
+            if(!set.contains(i)) map.put(i, map.getOrDefault(i,0)+1);
         }
 
         int max = Integer.MIN_VALUE;
         String res = "";
-        for (String i : map.keySet()) {
-            if (map.get(i) > max) {
+        for(String i : map.keySet()){
+            if(map.get(i) > max){
                 max = map.get(i);
                 res = i;
             }
--- src/relative_sort_array.java
+++ src/relative_sort_array.java
@@ -1,30 +1,40 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class relative_sort_array {
     public static void main(String[] args) {
-        int[] arr1 = {28, 6, 22, 8, 44, 17};
-        int[] arr2 = {22, 28, 8, 6};
+        int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+        int[] arr2 = {2, 1, 4, 3, 9, 6};
+        System.out.println(Arrays.toString(relativeSortArray(arr1, arr2)));
+    }
 
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> list_second_half = new ArrayList<>();
+        ArrayList<Integer> listForSecondHalf = new ArrayList<>();
+        int[] res = new int[arr1.length];
 
-        for (int j : arr2) {
-            for (int k : arr1) {
-                if (j == k) {
+        for (int i : arr1) map.put(i, map.getOrDefault(i, 0) + 1);
+        for (int k : arr2) {
+            int occurrence = map.get(k);
+            if (map.containsKey(k)) {
+                for (int j = 0; j < occurrence; j++) {
                     list.add(k);
                 }
             }
         }
-        for (int i : arr1) {
-            if (!list.contains(i)) {
-                list_second_half.add(i);
-            }
+        for(int i = 0; i < list.size(); i++){
+            res[i] = list.get(i);
         }
-        Collections.sort(list_second_half);
-        list.addAll(list_second_half);
-        int[] arr = list.stream().mapToInt(i -> i).toArray();
-        System.out.println(Arrays.toString(arr));
+        for(int k : map.keySet()){
+            if(!list.contains(k)) listForSecondHalf.add(k);
+        }
+        Collections.sort(listForSecondHalf);
+        for(int i = list.size(), j = 0; i < res.length; i++, j++){
+            res[i] = listForSecondHalf.get(j);
+        }
+        return res;
     }
 }
