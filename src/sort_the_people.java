4adbf84f8b3df0096c9d738fbe3a20794bae6f25
SharforazRahman <111514061+SharforazRahman@users.noreply.github.com>
2023-03-30 02:13:09 +0600

    Sort the People

--- src/sort_the_people.java
+++ src/sort_the_people.java
@@ -1,24 +1,24 @@
-import java.util.ArrayList;
+import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 
 public class sort_the_people {
     public static void main(String[] args) {
-        String[] names = {"IEO","Sgizfdfrims","QTASHKQ","Vk","RPJOFYZUBFSIYp","EPCFFt","VOYGWWNCf","WSpmqvb"};
-        int[] heights = {17233,32521,14087,42738,46669,65662,43204,8224};
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0, j = 0; i < names.length; i++, j++) {
-            list.add(heights[i] + names[i]);
+        String[] names = {"Mary", "John", "Emma"};
+        int[] heights = {180, 165, 170};
+        System.out.println(Arrays.toString(sortPeople(names, heights)));
+    }
+
+    public static String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> map = new HashMap<>();
+        String[] res = new String[names.length];
+        for (int i = 0; i < names.length; i++){
+            map.put(heights[i], names[i]);
         }
-        list.sort(Comparator.comparingInt(a -> Integer.parseInt(a.replaceAll("[a-zA-Z]", ""))));
-        StringBuilder str = new StringBuilder();
-        for(int i = list.size()-1; i >= 0; i--){
-            str.append(list.get(i));
+        Arrays.sort(heights);
+        for(int i = heights.length - 1, j = 0; i >= 0; i--,j++){
+            res[j] = map.get(heights[i]);
         }
-        str = new StringBuilder(str.toString().replaceAll("\\d", " "));
-        String r = str.toString().replaceAll("\\s+"," ").trim();;
-        String[] ans = r.split(" ");
-        System.out.println(Arrays.toString(ans));
+        return res;
     }
 }
